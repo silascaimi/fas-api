fb9bbdaa7a6bb9555a2eba0cf512d9b9a1e66a60
Silas Caimi <silascaimi@live.com>
2022-07-26 17:38:13 -0300

    Cria Model, Repository e Resource de categoria

---
+++ src/main/java/com/silascaimi/fasapi/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.silascaimi.fasapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.silascaimi.fasapi.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+}
