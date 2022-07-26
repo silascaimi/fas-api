8cd48916ff151797e8c05858528cf0cd025db793
Silas Caimi <silascaimi@live.com>
2022-07-26 20:39:40 -0300

    Retornar 404 caso não exista categoria

--- src/main/java/com/silascaimi/fasapi/resource/CategoriaResource.java
+++ src/main/java/com/silascaimi/fasapi/resource/CategoriaResource.java
@@ -2,6 +2,7 @@ package com.silascaimi.fasapi.resource;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -40,7 +41,11 @@ public class CategoriaResource {
 	}
 
 	@GetMapping("/{codigo}")
-	public Categoria buscarPeloCodigo(@PathVariable Long codigo) {
-		return categoriaRepository.findById(codigo).orElse(null);
+	public ResponseEntity<Categoria> buscarPeloCodigo(@PathVariable Long codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		if (categoria.isPresent()) {
+			return ResponseEntity.ok(categoria.get());
+		}
+		return ResponseEntity.notFound().build();
 	}
 }
