6076e96203828d8c9358c2df98e57b330bf2e91b
Silas Caimi <silascaimi@live.com>
2022-07-26 11:44:39 -0300

    fisrt commit

---
+++ src/main/java/com/silascaimi/fasapi/FasApiApplication.java
@@ -0,0 +1,13 @@
+package com.silascaimi.fasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FasApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FasApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/silascaimi/fasapi/FasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.silascaimi.fasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
