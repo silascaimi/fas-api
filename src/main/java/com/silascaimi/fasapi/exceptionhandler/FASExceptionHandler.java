91ad544cd84732474a3b5334853e195df990455c
Silas Caimi <silascaimi@live.com>
2022-07-27 12:32:47 -0300

    Adicionando propriedades a mensagem de erro

--- src/main/java/com/silascaimi/fasapi/exceptionhandler/FASExceptionHandler.java
+++ src/main/java/com/silascaimi/fasapi/exceptionhandler/FASExceptionHandler.java
@@ -20,10 +20,40 @@ public class FASExceptionHandler extends ResponseEntityExceptionHandler {
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		
+
 		String mensagemUsuario = messageSource.getMessage("mensagem.invalida", null, LocaleContextHolder.getLocale());
-		
-		return handleExceptionInternal(ex, mensagemUsuario, headers, HttpStatus.BAD_REQUEST, request);
+
+		String mensagemDesenvolvedor = ex.getCause().toString();
+
+		return handleExceptionInternal(ex, new Erro(mensagemUsuario, mensagemDesenvolvedor), headers,
+				HttpStatus.BAD_REQUEST, request);
 	}
 
+	public static class Erro {
+
+		private String mensagemUsuario;
+		private String mensagemDesenvolvedor;
+
+		public Erro(String mensagemUsuario, String mensagemDesenvolvedor) {
+			this.mensagemUsuario = mensagemUsuario;
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+		public String getMensagemUsuario() {
+			return mensagemUsuario;
+		}
+
+		public void setMensagemUsuario(String mensagemUsuario) {
+			this.mensagemUsuario = mensagemUsuario;
+		}
+
+		public String getMensagemDesenvolvedor() {
+			return mensagemDesenvolvedor;
+		}
+
+		public void setMensagemDesenvolvedor(String mensagemDesenvolvedor) {
+			this.mensagemDesenvolvedor = mensagemDesenvolvedor;
+		}
+
+	}
 }
