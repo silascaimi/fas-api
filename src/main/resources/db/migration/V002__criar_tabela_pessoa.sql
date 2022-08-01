573b01fdb899c3fb23d191e706d7fba62065ffb8
Silas Caimi <silascaimi@live.com>
2022-08-01 20:20:49 -0300

    Desconsiderar: ajustes nas migrations

--- src/main/resources/db/migration/V002__criar_tabela_pessoa.sql
+++ src/main/resources/db/migration/V002__criar_tabela_pessoa.sql
@@ -11,20 +11,13 @@ CREATE TABLE pessoa (
 	ativo BOOLEAN NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO pessoa
-	(nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
-values
-	('João Felipe Scollari', 1, 'Rua Eduardo Guimarães', '906', '"1440 Aenean Rd', 'Mauris Av.', '67123-411', 'Campinas', 'Minas Gerais');
-INSERT INTO pessoa
-	(nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
-values
-	('Sade Ingra', 1, '278-2132 Mi. Street', '397', 'P.O. Box 253, 928 Pretium Rd.', 'Non, Rd.', '58086-622', 'Juiz de For', 'Goiás');
-INSERT INTO pessoa
-	(nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
-values
-	('Chava Robinson', 1, '405-7279 Est Ave', '519', '933-5890 Vulputate St', 'Mauris Av.', '67123-411', 'Anápolis', 'Paraíba');
-INSERT INTO pessoa
-	(nome, ativo, logradouro, numero, complemento, bairro, cep, cidade, estado)
-values
-	('Priscilla Burns', 1, '279-4399 Nulla Rd.', '927', '595-1722 Consectetuer St', 'Nibh St.', '88808-374', 'Belém', 'Ceará');
-	
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('João Silva', 'Rua do Abacaxi', '10', null, 'Brasil', '38.400-12', 'Uberlândia', 'MG', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Maria Rita', 'Rua do Sabiá', '110', 'Apto 101', 'Colina', '11.400-12', 'Ribeirão Preto', 'SP', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Pedro Santos', 'Rua da Bateria', '23', null, 'Morumbi', '54.212-12', 'Goiânia', 'GO', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Ricardo Pereira', 'Rua do Motorista', '123', 'Apto 302', 'Aparecida', '38.400-12', 'Salvador', 'BA', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Josué Mariano', 'Av Rio Branco', '321', null, 'Jardins', '56.400-12', 'Natal', 'RN', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Pedro Barbosa', 'Av Brasil', '100', null, 'Tubalina', '77.400-12', 'Porto Alegre', 'RS', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Henrique Medeiros', 'Rua do Sapo', '1120', 'Apto 201', 'Centro', '12.400-12', 'Rio de Janeiro', 'RJ', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Carlos Santana', 'Rua da Manga', '433', null, 'Centro', '31.400-12', 'Belo Horizonte', 'MG', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Leonardo Oliveira', 'Rua do Músico', '566', null, 'Segismundo Pereira', '38.400-00', 'Uberlândia', 'MG', true);
+INSERT INTO pessoa (nome, logradouro, numero, complemento, bairro, cep, cidade, estado, ativo) values ('Isabela Martins', 'Rua da Terra', '1233', 'Apto 10', 'Vigilato', '99.400-12', 'Manaus', 'AM', true);
