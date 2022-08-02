6eee5d0ab2e7c4c5d0d2abc48f8e7190c0f6b2a9
Silas Caimi <silascaimi@live.com>
2022-08-01 23:39:51 -0300

    Configurando o TimeZone do projeto para UTC

--- src/main/java/com/silascaimi/fasapi/FasApiApplication.java
+++ src/main/java/com/silascaimi/fasapi/FasApiApplication.java
@@ -1,5 +1,9 @@
 package com.silascaimi.fasapi;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +13,10 @@ public class FasApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(FasApiApplication.class, args);
 	}
+	
+	@PostConstruct
+	private void started() {
+	    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+	  }
 
 }
