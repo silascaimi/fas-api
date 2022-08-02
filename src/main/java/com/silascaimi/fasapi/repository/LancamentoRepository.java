e00896c97a17cb925cbb913eeb7fb33b8e8cabee
Silas Caimi <silascaimi@live.com>
2022-08-01 23:20:07 -0300

    Cria LancamentoRepository e LancamentoResource

---
+++ src/main/java/com/silascaimi/fasapi/repository/LancamentoRepository.java
@@ -0,0 +1,9 @@
+package com.silascaimi.fasapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.silascaimi.fasapi.model.Lancamento;
+
+public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
+
+}
