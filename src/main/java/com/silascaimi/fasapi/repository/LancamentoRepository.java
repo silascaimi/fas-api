368ba716f4a54b02874a8df7f6b1a453bfaf4d29
Silas Caimi <silascaimi@live.com>
2022-08-01 23:25:31 -0300

    Contornando vários selects com fetch join

--- src/main/java/com/silascaimi/fasapi/repository/LancamentoRepository.java
+++ src/main/java/com/silascaimi/fasapi/repository/LancamentoRepository.java
@@ -1,9 +1,18 @@
 package com.silascaimi.fasapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.silascaimi.fasapi.model.Lancamento;
 
 public interface LancamentoRepository extends JpaRepository<Lancamento, Long> {
 
+	 @Query("    select l " +
+	           "      from Lancamento l " +
+	           "join fetch l.categoria c " +
+	           "join fetch l.pessoa p ")
+	    List<Lancamento> findAll();
+	 
 }
