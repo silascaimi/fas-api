c902f999e5b61cf17154f5fb45f52738b7dd1d73
Silas Caimi <silascaimi@live.com>
2022-08-23 21:56:08 -0300

    usando o thymeleaf para enviar email com template

--- src/main/java/com/silascaimi/fasapi/mail/AppMailSender.java
+++ src/main/java/com/silascaimi/fasapi/mail/AppMailSender.java
@@ -1,7 +1,10 @@
 package com.silascaimi.fasapi.mail;
 
 //import java.util.Arrays;
+//import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
@@ -12,12 +15,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+//import com.silascaimi.fasapi.model.Lancamento;
+//import com.silascaimi.fasapi.repository.LancamentoRepository;
 
 @Component
 public class AppMailSender {
 
 	@Autowired
 	private JavaMailSender mailSender;
+	
+	@Autowired
+	private TemplateEngine thymeleaf;
+	
+//	@Autowired
+//	private LancamentoRepository lancamentoRepository;
 	
 	/* Teste de envio de email simples
 	 * 
@@ -26,6 +40,23 @@ public class AppMailSender {
 	 * Arrays.asList("destinatario@live.com"), "Teste de email", "Teste OK"); }
 	 */
 	
+	// Teste de envio de email com template
+//	@EventListener
+//	public void teste(ApplicationReadyEvent event) {
+//		String template = "mail/aviso-lancamentos-vencidos";
+//		
+//		List<Lancamento> lista = lancamentoRepository.findAll();
+//		
+//		Map<String, Object> variaveis = new HashMap<>();
+//		variaveis.put("lancamentos", lista);
+//		
+//		this.enviarEmail("silascaimi.br@gmail.com", 
+//				Arrays.asList("silascaimi@live.com"), 
+//				"Teste de email", 
+//				template, variaveis);
+//	}
+	
+	// Email simples
 	public void enviarEmail(String remetente, List<String> destinatarios, String assunto, String mensagem) {
 		MimeMessage mimeMessage = mailSender.createMimeMessage();
 		
@@ -40,5 +71,17 @@ public class AppMailSender {
 		} catch (MessagingException e) {
 			throw new RuntimeException("Problema com o envio de email!", e);
 		}
+	}
+	
+	// Email com template thymeleaf
+	public void enviarEmail(String remetente, List<String> destinatarios, String assunto, String template, 
+			Map<String, Object> variaveis) {
+		Context context = new Context(new Locale("pt", "BR"));
+		
+		variaveis.entrySet().forEach(e -> context.setVariable(e.getKey(), e.getValue()));
+		
+		String mensagem = thymeleaf.process(template, context);
+		
+		this.enviarEmail(remetente, destinatarios, assunto, mensagem);
 	}
 }
