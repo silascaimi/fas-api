913caa2a79a8db694f7f898286656cb2620f6198
Silas Caimi <silascaimi@live.com>
2022-08-30 20:32:07 -0300

    add comment

--- src/main/java/com/silascaimi/fasapi/model/Pessoa.java
+++ src/main/java/com/silascaimi/fasapi/model/Pessoa.java
@@ -10,7 +10,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -68,7 +67,7 @@ public class Pessoa {
 	}
 	
 	@JsonIgnore // Faz o Jackson ignorar na serialização do json
-	@Transient // Faz o hibernate ignorar propriedade
+	//@Transient // Faz o hibernate ignorar propriedade quando usado mapeamento Property Access Mode (pelos getters)
 	public Boolean isInativo() {
 		log.info("Status {}", ativo);
 		return !ativo;
