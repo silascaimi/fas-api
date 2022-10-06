5834c566318f8f07cb13780ae6b3944388324d6a
Silas Caimi <silascaimi@live.com>
2022-10-06 14:58:16 -0300

    lançando exceção sem uso da bilioteca commons lang3

--- src/main/java/com/silascaimi/fasapi/exceptionhandler/FASExceptionHandler.java
+++ src/main/java/com/silascaimi/fasapi/exceptionhandler/FASExceptionHandler.java
@@ -67,11 +67,10 @@ public class FASExceptionHandler extends ResponseEntityExceptionHandler {
 	@ExceptionHandler({ DataIntegrityViolationException.class })
 	public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex, WebRequest request) {
 		String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitida", null, LocaleContextHolder.getLocale());
-		String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
-		// String mensagemDesenvolvedor = ex.getRootCause().toString(); sem  usar biblioteca commons.lang3
+		//String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+		String mensagemDesenvolvedor = ex.getMostSpecificCause().toString(); //sem  usar biblioteca commons.lang3
 		List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 
-		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
 		return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.UNPROCESSABLE_ENTITY, request);
 	}
 
